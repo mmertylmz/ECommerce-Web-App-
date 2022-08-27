da4a8494eb0725230be1f8ade3acbbca54eeafbf
mmertylmz <mertcnylmz0698@gmail.com>
2022-08-27 14:18:16 +0300

    v4 -Added EF Core Tracking Performance Optimization

--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -21,13 +21,18 @@ namespace ECommerceAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new(){Id=Guid.NewGuid(), Name="Product 1", CreatedDate=DateTime.UtcNow, Price=100, Stock=10},
-                new(){Id=Guid.NewGuid(), Name="Product 2", CreatedDate=DateTime.UtcNow, Price=120, Stock=30},
-                new(){Id=Guid.NewGuid(), Name="Product 3", CreatedDate=DateTime.UtcNow, Price=150, Stock=20}
-            });
-            var count = await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new(){Id=Guid.NewGuid(), Name="Product 1", CreatedDate=DateTime.UtcNow, Price=100, Stock=10},
+            //    new(){Id=Guid.NewGuid(), Name="Product 2", CreatedDate=DateTime.UtcNow, Price=120, Stock=30},
+            //    new(){Id=Guid.NewGuid(), Name="Product 3", CreatedDate=DateTime.UtcNow, Price=150, Stock=20}
+            //});
+            //var count = await _productWriteRepository.SaveAsync();
+
+            //Product p = await _productReadRepository.GetByIdAsync("de66c59a-2db1-4afd-8113-b5755dc19691"); //default true
+            Product p = await _productReadRepository.GetByIdAsync("de66c59a-2db1-4afd-8113-b5755dc19691",false); //change will not be affected
+            p.Name = "Mert";
+            await _productWriteRepository.SaveAsync();
         }
 
         [HttpGet("{id}")]
